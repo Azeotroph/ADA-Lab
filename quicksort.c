64d5f295f47f4228f1727305a17452ec00aa67b9
Azeotroph <118718128+Azeotroph@users.noreply.github.com>
2023-08-28 02:26:26 +0530

    Create quicksort.c

---
+++ quicksort.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include<time.h>
+clock_t start, end;
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+int partition(int array[], int low, int high) {
+
+  int pivot = array[high];
+  int i = (low - 1);
+
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+            i++;
+
+      swap(&array[i], &array[j]);
+    }
+  }
+
+
+  swap(&array[i + 1], &array[high]);
+
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+  }
+}
+
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+
+int main() {
+  int arr_size;
+  int data[10];
+
+  printf("Enter the number of elements:");
+  scanf("%d",&arr_size);
+
+  printf("enter the elements :\n");
+  for(int i=0; i<arr_size; i++){
+    scanf("%d",&data[i]);
+  }
+
+
+
+
+  printf("Unsorted Array\n");
+  printArray(data, arr_size);
+  start=clock();
+  quickSort(data, 0, arr_size - 1);
+  end=clock();
+  double t=((double)end-start)/CLOCKS_PER_SEC;
+  printf("Sorted array in ascending order: \n");
+  printArray(data, arr_size);
+  printf("Time Taken: %lf",t);
+  return 0;
+}
